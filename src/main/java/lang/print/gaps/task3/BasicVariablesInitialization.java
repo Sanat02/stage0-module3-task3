9272e9f0a335edc8e9fd5cd1305ef5c378029ee3
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-27 17:47:05 +0600

    Update BasicVariablesInitialization.java

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,11 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int var1=1;
+        int var2=10;
+        int var3=100;
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
     }
 }
